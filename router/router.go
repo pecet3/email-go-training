474459fb0ac9a36a08444bae530bebce91c5640a
pecet3 <pecet3107@gmail.com>
2023-11-24 01:04:16 +0100

    init

---
+++ router/router.go
@@ -0,0 +1,19 @@
+package router
+
+import (
+	"log"
+	"mails/controllers"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func SetupAndRun() {
+	mux := mux.NewRouter()
+	port := ":6660"
+
+	mux.HandleFunc("/email", controllers.AddEmail).Methods("POST")
+
+	log.Println("Starting the server at port", port)
+	log.Fatal(http.ListenAndServe(port, mux))
+}
