09e411d903251b6d346118ee428e6e09ca0f71c5
pecet3 <pecet3107@gmail.com>
2023-11-24 01:46:35 +0100

    methods for email

--- router/router.go
+++ router/router.go
@@ -12,7 +12,7 @@ func SetupAndRun() {
 	mux := mux.NewRouter()
 	port := ":6660"
 
-	mux.HandleFunc("/email", controllers.AddEmail).Methods("POST")
+	mux.HandleFunc("/email", controllers.AddEmail).Methods("POST", "GET")
 
 	log.Println("Starting the server at port", port)
 	log.Fatal(http.ListenAndServe(port, mux))
